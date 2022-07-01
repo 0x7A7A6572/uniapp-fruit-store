434f36849f6c5ac70823b624b3ce16699c9cdd0f
0x7A7A6572 <2623385294@qq.com>
2022-07-01 14:22:46 +0800

    onluanch 添加获取系统信息和本地用户信息

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,48 @@
 <script>
+import { useStore } from "vuex";
 export default {
   onLaunch: function () {
+    const store = useStore();
+    /** 获取一些系统信息 用于组件样式 */
+    uni.getSystemInfo({
+      success: (e) => {
+        let statusBar = 0;
+        let customBar = 0;
+        // #ifdef MP-WEIXIN
+        statusBar = e.statusBarHeight;
+        // @ts-ignore
+        const custom = wx.getMenuButtonBoundingClientRect();
+        customBar = custom.bottom + custom.top - e.statusBarHeight;
+        // #endif
+        console.log("customBar:", customBar, " statusBar:", statusBar);
+        store.commit("updatedCustomBar", customBar);
+        store.commit("updatedStatusBar", statusBar);
+      },
+    });
+
+    /* 尝试读取本地数据 */
+    uni.getStorage({
+      key: "userInfo",
+      success: (res) => {
+        console.log("read localstorage userdata success>",res);
+        store.commit("updatedUserBaseInfo", {
+          avatarUrl: res.data.avatarUrl,
+          nickName: res.data.nickName,
+        });
+        setTimeout(()=>{
+        //  store.commit("updatedDrawerAction",false);
+          uni.showToast({
+            title: '欢迎回来' + res.data.nickName,
+            duration: 1000,
+            icon: "none"
+          });
+        },1500)
+      },
+      fail: (fail) => {
+        console.log("localstorage not have userdata>");
+        // 本地无用户数据缓存 需要后续点击头像登录
+      },
+    });
     console.log("App Launch");
   },
   onShow: function () {
@@ -17,5 +59,12 @@ export default {
 @import "colorui/main.css";
 @import "colorui/icon.css";
 /* 自定义的全局样式*/
-@import "./index.css";
+/* @import "index.css"; */
+page,
+view {
+  --mainColor: #ff430a;
+  --pageBackgroundColor: linear-gradient(45deg, #ff9700, #ed1c24);
+  --navBackgroundColor: linear-gradient(180deg, #ff9700, #ff970000);
+  --disableTextColor: grey;
+}
 </style>
