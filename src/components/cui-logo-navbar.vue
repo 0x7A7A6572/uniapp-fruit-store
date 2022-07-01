1594987dc60c9b665f4287f84f7ba15e3c7c0741
0x7A7A6572 <2623385294@qq.com>
2022-07-01 14:34:55 +0800

    格式化和清理一些注释 配置更新

--- src/components/cui-logo-navbar.vue
+++ src/components/cui-logo-navbar.vue
@@ -1,41 +1,66 @@
 <template>
-	<view>
-		<image class='logo margin-top-xl'  :src="logo"
-			mode="heightFix">
-		</image>
-		<slot></slot>
-	</view>
+  <view
+    class="cui-log-nav"
+    :style="{ height: height + 'px', paddingTop: statusBarHeight + 'px' }"
+  >
+    <text v-if="isBack" class="cuIcon-back navbar-func" @tap="BackPage"></text>
+    <image v-if="logo != null" class="logo margin-top-xl" :src="logo" mode="heightFix">
+    </image>
+	<slot  class="navbar-func"></slot>
+  </view>
 </template>
 
 <script>
-	export default {
-		name: "cui-logo-navbar",
-		data() {
-			return {
+export default {
+  name: "cui-logo-navbar",
+  data() {
+    return {};
+  },
+  props: {
+    logo: {
+      type: String,
+    },
+    isBack: {
+      type: Boolean,
+    },
+    height: {
+      type: Number,
+    },
+    statusBarHeight: {
+      type: Number,
+    },
+  },
+  methods: {
+    NavChange: function (e) {
+      this.PageCur = e.currentTarget.dataset.cur;
+    },
 
-			};
-		},
-		props: {
-			logo: {
-				type: String
-			}
-		},
-		methods: {
-			NavChange: function(e) {
-				this.PageCur = e.currentTarget.dataset.cur
-			}
-		}
-	}
+    BackPage() {
+      uni.navigateBack();
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-	.logo {
-		position: fixed;
-		z-index: 1;
-		height: 5vh;
-		width: 100vw;
-		margin-left: 2vw;
-	}
-
+<style>
+.cui-log-nav {
+  width: 100vw;
+  z-index: 1;
+  background: linear-gradient(180deg, #ff9700, #ff970000);
+  /* height: 60px; */
+  /* padding: 0.5em; */
+  /* padding-top: var(--status-bar-height); */
+}
+.cui-log-nav .logo {
+  position: absolute;
+  height: 5vh;
+  width: 100vw;
+  margin-left: 2vw;
+  z-index: 1;
+}
+.navbar-func {
+  font-size: 2em;
+  color: white;
+  padding: 0.5em;
+}
 </style>
--- src/components/cui-user-drawer.vue
+++ src/components/cui-user-drawer.vue
@@ -1,118 +1,169 @@
 <template>
-	<view class="cu-list menu card-menu shadow-lg">
-		<view class="user-info">
-			<view class="head-imge cu-avatar xl text-xsl round ">
-				<button class=" cu-avatar xl round " style="width:100%;height:100%" open-type="getUserInfo"
-					bindgetuserinfo="onGetUserInfo"
-					:style="{backgroundImage: 'url(' + userInfo.avatarUrl +')'}"></button>
-				<view :class="'cu-tag  badge cuIcon-crownfill ' + (userInfo.vip ? 'bg-yellow' : 'bg-grey') ">
-				</view>
-			</view>
-			<view class="user-name">
-				<text class="text-white text-bold text-xxl">{{userInfo.nickName}} </text>
-			</view>
-
-
-		</view>
-		<view class="cu-list menu user-list sm-border card-menu">
-			<view v-for="item in menuList" :class="'cu-item ' + isPageAction(item.pageUrl)" @click="pageChange(item.pageUrl)" :key="item.text">
-				<view class="content">
-					<text :class="[item.icon, getItemColor(item.color)]" ></text>
-					<text :class="getItemColor(item.color)">{{item.text}}</text>
-				</view>
-			</view>
-			<slot></slot>
-		</view>
-	</view>
+  <view class="cu-list menu card-menu shadow-lg">
+    <view class="user-info">
+      <view class="head-imge cu-avatar xl text-xsl round">
+        <button
+          class="cu-avatar xl round"
+          :style="{ backgroundImage: 'url(' + userInfo.avatarUrl + ')' }"
+          @tap="toLogin"
+        ></button>
+        <view
+          :class="
+            'cu-tag  badge cuIcon-crownfill ' + (userInfo.vip ? 'bg-yellow' : 'bg-grey')
+          "
+        >
+        </view>
+      </view>
+      <view class="user-name">
+        <text class="text-white text-bold text-xxl">{{ userInfo.nickName }} </text>
+      </view>
+    </view>
+    <view class="cu-list menu user-list sm-border card-menu">
+      <view
+        v-for="item in menuList"
+        :class="'cu-item ' + isPageAction(item.pageUrl)"
+        @click="pageChange(item.pageUrl)"
+        :key="item.text"
+      >
+        <view class="content">
+          <text :class="[item.icon, getItemColor(item.color)]"></text>
+          <text :class="getItemColor(item.color)">{{ item.text }}</text>
+        </view>
+      </view>
+      <slot></slot>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		name: "cui-user-drawer",
-		data() {
-			return {
+import { useStore } from "vuex";
+import login from "@/api/login";
 
-			};
-		},
-		computed: {
-			isPageAction() {
-				return (pageurl) => {
-					return pageurl == null ? "" : "arrow";
-				}
-			},
-			getItemColor() {
-				return (color)=>{
-					return color == null ? "text-grey" : color;
-				}
-			}
-		},
-		methods: {
-			pageChange: function(pageurl) {
-				console.log("NavChange ->",pageurl);
-				uni.navigateTo({
-					url: "/pages/" + pageurl + "/" + pageurl
-					});
-			}
-		},
-		props: {
-			menuList: {
-				type: Array
-			},
-			userInfo: {
-				type: Object,
-				default: {
-					type: 'admin',
-					vip: true,
-					avatarUrl: '/static/images/avatar.png',
-					nickName: '未登录',
-					loveGoods: '暂无喜欢的商品',
-					oderrs: [{
-						orderId: "订单id",
-						orderStatu: "订单状态",
-						waybillNumber: "KD0000000000000",
-						goodsList: [{
-							goodsId: 1,
-							buyCount: 1
-						}]
-					}],
-					adds: [{
-						name: "方先生",
-						addr: "xx市xx区xx小区xx号",
-						namber: "13888888888"
-					}],
-					coupons: [{
-						id: "优惠券id",
-						name: "满200减20优惠券",
-						full: 200,
-						reduction: 20
-					}]
-				}
-			}
-		}
-	}
+export default {
+  name: "cui-user-drawer",
+  setup() {
+    const store = useStore();
+
+    function isPageAction(pageurl) {
+      return pageurl == null ? "" : "arrow";
+    }
+    function getItemColor(color) {
+      return color == null ? "text-grey" : color;
+    }
+    function pageChange(pageurl) {
+      console.log("NavChange ->", pageurl);
+      uni.navigateTo({
+        url: "/pages/" + pageurl + "/" + pageurl,
+      });
+    }
+    function toLogin() {
+      try {
+        let userinfo = uni.getStorageSync("userInfo");
+        if (userinfo) {
+          console.log("toLogin >userinfo:",userinfo)
+          // login.do((res) => {
+          //   console.log(">>>>", res);
+          // });
+          store.commit("updatedUserBaseInfo", {
+            avatarUrl: userinfo.avatarUrl,
+            nickName: userinfo.nickName,
+          });
+        } else {
+          login.do((res) => {
+            console.log(">>>>", res);
+          });
+          login.getUserProfile((res) => {
+            console.log("getUserProfile", res);
+            store.commit("updatedUserBaseInfo", {
+              avatarUrl: res.userInfo.avatarUrl,
+              nickName: res.userInfo.nickName,
+            });
+          });
+        }
+      } catch (e) {
+        // error
+        console.error("getStorageSync error:", e);
+      }
+    }
+
+    return {
+      isPageAction,
+      getItemColor,
+      pageChange,
+      toLogin,
+    };
+  },
+
+  props: {
+    menuList: {
+      type: Array,
+    },
+    userInfo: {
+      type: Object,
+      default: {
+        type: "admin",
+        vip: true,
+        avatarUrl: "/static/images/avatar.png",
+        nickName: "未登录",
+        loveGoods: "暂无喜欢的商品",
+        oderrs: [
+          {
+            orderId: "订单id",
+            orderStatu: "订单状态",
+            waybillNumber: "KD0000000000000",
+            goodsList: [
+              {
+                goodsId: 1,
+                buyCount: 1,
+              },
+            ],
+          },
+        ],
+        adds: [
+          {
+            name: "方先生",
+            addr: "xx市xx区xx小区xx号",
+            namber: "13888888888",
+          },
+        ],
+        coupons: [
+          {
+            id: "优惠券id",
+            name: "满200减20优惠券",
+            full: 200,
+            reduction: 20,
+          },
+        ],
+      },
+    },
+  },
+  components: {
+    useStore,
+  },
+};
 </script>
 
 <style>
-	.user-info {
-		text-align: center;
-	}
+.user-info {
+  text-align: center;
+}
 
-	.user-info .head-imge {
-		margin-top: 10vh;
-	}
+.user-info .head-imge {
+  margin-top: 10vh;
+}
 
-	.user-list {
-		margin-top: 5vh;
-	}
+.user-list {
+  margin-top: 5vh;
+}
 
-	.user-name {
-		overflow: hidden;
-		text-overflow: ellipsis;
-		width: 50vw;
-		margin: 0 auto;
-	}
+.user-name {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 50vw;
+  margin: 0 auto;
+}
 
-	.user-name text {
-		white-space: nowrap;
-	}
+/* .user-name text {
+  white-space: nowrap;
+} */
 </style>
