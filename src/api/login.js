2e5f6fcc1935d0d51b33ba94ab2ece5490432d0b
0x7A7A6572 <2623385294@qq.com>
2022-07-01 14:28:30 +0800

    添加连接后台获取用户信息逻辑

--- src/api/login.js
+++ src/api/login.js
@@ -1,24 +1,55 @@
+const GET_USER_INFO = "https://service.zzerx.cn";
 export default {
-	do: function (callback) {
+	do: function (callback,showLog) {
 		uni.login({
 			provider: 'weixin',
 			success: function (loginRes) {
-				console.log(loginRes.authResult);
-				// 获取用户信息
-				uni.getUserInfo({
-					provider: 'weixin',
-					success: function (infoRes) {
-						console.log('用户昵称为：' + infoRes.userInfo.nickName);
-						callback({ statu: true, msg: "uni.login success,getUserInfo success" }, loginRes, infoRes);
+				if(showLog){
+				console.log("login code:",loginRes.code);
+				}
+				uni.request({
+					url: GET_USER_INFO, 
+					header: {
+						'Content-Type': 'application/x-www-form-urlencoded'  
+					 }, 
+					data: {
+						type: "fruitStore-login",
+						code: loginRes.code,
+						client: "@uniapp/mp-weixin"
 					},
-					fail: function(){
-						callback({ statu: false, msg: "getUserInfo fail" }, loginRes, infoRes);
+					method: "POST",
+					success: (res) => {
+						if(showLog){
+							console.log("POST success:",res.data);
+						}
+						callback(res.data)
 					}
 				});
 			},
 			fail: function () {
 				callback({ statu: false, msg: "uni.login fail" });
 			}
+		});
+	},
+	getUserProfile: function (callback,showLog) {
+		uni.getUserProfile({
+			desc: "登录后可同步数据",
+			lang: "zh_CN",
+			success: (res) => {
+
+				callback(res);
+			},
+			fail: (fail) => {
+				uni.showToast({
+					title: "登录失败",
+					icon: "error",
+					duration: 1000,
+				});
+				if(showLog){
+				console.log("getUserProfile fail:", fail);
+				}
+				callback(fail);
+			},
 		});
 	}
 }
