ea085881893d9c4426ddcc9755844919bd722d13
0x7A7A6572 <2623385294@qq.com>
2022-07-03 23:06:57 +0800

    添加跳转页面默认页面

---
+++ src/pages/feedback/feedback.vue
@@ -0,0 +1,27 @@
+<template>
+  <view>
+    <cui-logo-navbar
+      class="logo-nav"
+      :height="navHeight"
+      :statusBarHeight="statusBarHeight"
+      :isBack="true"
+    >
+    </cui-logo-navbar>
+  </view>
+</template>
+
+<script>
+import cuiLogoNavbar from "@/components/cui-logo-navbar.vue";
+import { useStore } from "vuex";
+export default {
+  components: { cuiLogoNavbar },
+  setup() {
+    return {
+      navHeight: useStore().getters.nvaHeight,
+      statusBarHeight: useStore().getters.statusBar,
+    };
+  },
+};
+</script>
+
+<style></style>
