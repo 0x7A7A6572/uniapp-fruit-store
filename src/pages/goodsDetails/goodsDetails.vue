6910557fa49a9a315950c31af68d6f2b95416587
0x7A7A6572 <2623385294@qq.com>
2022-07-11 14:26:55 +0800

    商品详情页的加购

--- src/pages/goodsDetails/goodsDetails.vue
+++ src/pages/goodsDetails/goodsDetails.vue
@@ -14,12 +14,7 @@
       :src="goods.imgMain"
       mode="widthFix"
     />
-    <video
-      v-else
-      :show-fullscreen-btn="true"
-      :src="goods.video"
-      object-fit="cover"
-    />
+    <video v-else :show-fullscreen-btn="true" :src="goods.video" object-fit="cover" />
 
     <view class="cu-list menu sm-border card-menu margin bg-white">
       <view class="cu-item">
@@ -32,14 +27,18 @@
       <view class="cu-item">
         <view class="content">
           <text class="goods-style-desc text-grey">价格：</text>
-          <text class="goods-style-select padding-left text-orange text-bold">￥{{ goods.goodsPrice }}</text>
+          <text class="goods-style-select padding-left text-orange text-bold"
+            >￥{{ goods.goodsPrice }}</text
+          >
         </view>
       </view>
 
       <view class="cu-item">
         <view class="content">
           <text class="goods-style-desc text-grey">规格：</text>
-          <text class="goods-style-select padding-left">{{ goods.goodsWeight + '斤/' + goods.goodsPack }}</text>
+          <text class="goods-style-select padding-left">{{
+            goods.goodsWeight + "斤/" + goods.goodsPack
+          }}</text>
         </view>
       </view>
 
@@ -59,17 +58,30 @@
 
     <view class="flxed-bottom cu-bar bg-white tabbar border shop text-larger">
       <view class="action text-red"> <view class="cuIcon-likefill"></view> 已收藏 </view>
-      <view class="action">
+      <view class="action" @click.stop="gotoShopingcart">
         <view class="cuIcon-cart">
-          <view class="cu-tag badge">{{shopingcartLenght}}</view>
+          <!-- <view class="cu-tag badge">{{ shopingcartLenght }}</view> -->
         </view>
         购物车
       </view>
       <view class="btn-group">
-        <button class="cu-btn bg-orange round shadow-blur">加入购物车</button>
+        <button class="cu-btn bg-orange round shadow-blur" @click.stop="tobuy(goods)">
+          加入购物车
+        </button>
         <button class="cu-btn bg-red round shadow-blur">立即购买</button>
       </view>
     </view>
+    <!-- 点击购物后出现的弹窗 -->
+    <cui-shoping-cart-dialog
+      v-if="shopingCartDialogShow"
+      v-model:show="shopingCartDialogShow"
+      :item="goods"
+      :orgiinStock="orgiinStock"
+      :onSelectCouponClick="onSelectCouponClick"
+      :onSelectAddrClick="onSelectAddrClick"
+      :onClickAddShopingcart="onClickAddShopingcart"
+      :onClickBuyNow="onClickBuyNow"
+    />
   </view>
 </template>
 
@@ -78,26 +90,117 @@ import cuiLogoNavbar from "@/components/cui-logo-navbar.vue";
 import cuiRotationChart from "@/components/cui-rotation-chart.vue";
 import combButton from "@/components/comb-button.vue";
 import cuiCounter from "@/components/cui-counter.vue";
+import cuiShopingCartDialog from "@/components/cui-shoping-cart-dialog.vue";
 import { ref } from "vue";
 import { useStore } from "vuex";
 import { onLoad } from "@dcloudio/uni-app";
 export default {
-  components: { cuiLogoNavbar, cuiRotationChart, combButton, cuiCounter },
+  components: {
+    cuiLogoNavbar,
+    cuiRotationChart,
+    combButton,
+    cuiCounter,
+    cuiShopingCartDialog,
+  },
   setup() {
     const store = useStore();
     let goods = ref({});
     let swiperImage = ref([]);
-    let shopingcartLenght = store.getters.shoppingCart.length; 
+    let shopingcartLenght = store.getters.shoppingCart.length;
+
+    /** 加购 购买弹窗数据 */
+    let orgiinStock = ref(0);
+    let shopingCartDialogShow = ref(false);
+
     onLoad((options) => {
       console.log("上一个界面传过来的值：", options);
       goods.value = store.getters.goodsListById(options.id);
       swiperImage.value = goods.value.imgDetails;
     });
+
+
+    /** 以下与index.vue 重复*/
+
+    // 打开购物车页面
+    function gotoShopingcart() {
+      if (!isLogin(true)) {
+        return;
+      }
+      console.log("NavChange -> gotoShopingcart");
+      uni.navigateTo({
+        url: "/pages/shopingcart/shopingcart",
+      });
+    }
+
+    /** 打开购买弹窗 */
+    function tobuy(item) {
+      if (!isLogin(true)) {
+        return;
+      }
+      console.log("show shoping cart", item.id);
+      shopingCartDialogShow.value = true;
+      orgiinStock.value = item.stock;
+    }
+
+    /* 选择优惠券 */
+    function onSelectCouponClick(totalMoney) {
+      if (!isLogin(false)) {
+        return;
+      }
+      console.log("i ckick SelectCoupon", totalMoney);
+      uni.navigateTo({
+        url: "/pages/coupon/coupon?msg=onSelectCouponClick&totalMoney=" + totalMoney,
+      });
+    }
+    /* 选择发货地址 */
+    function onSelectAddrClick() {
+      console.log("i ckick SelectAddr");
+      uni.navigateTo({
+        url: "/pages/addrAdmin/addrAdmin?msg=onSelectAddrClick",
+      });
+    }
+
+    /** 购物dialog 的添加进购物车按钮和立即购买按钮*/
+    function onClickAddShopingcart(shopingcart) {
+      console.log("i need add to shopingcart", shopingcart);
+      store.commit("addShopingcart", shopingcart);
+      //关闭弹窗
+      shopingCartDialogShow.value = false;
+      uni.showToast({
+        title: "已加入购物车",
+      });
+    }
+
+    function onClickBuyNow(shopingcart) {
+      console.log("goto buy now");
+    }
+
+        /** 判断是否登录 */
+    function isLogin(toChangeDrawer) {
+      if (store.getters.userInfo.id != null) {
+        //check cookice
+        return true;
+      } else {
+        indexToast("请先登录", 1000, "error");
+        if (toChangeDrawer) {
+          changeDrawer();
+        }
+        return false;
+      }
+    }
+
     return {
-      goods,shopingcartLenght,
+      goods,
+      shopingcartLenght,
       swiperImage,
       navHeight: useStore().getters.nvaHeight,
       statusBarHeight: useStore().getters.statusBar,
+      orgiinStock,shopingCartDialogShow,
+      tobuy,gotoShopingcart,
+      onSelectCouponClick,
+      onSelectAddrClick,
+      onClickAddShopingcart,
+      onClickBuyNow,
     };
   },
 };
@@ -112,7 +215,7 @@ export default {
 }
 
 uni-video {
-    width: 100% !important;
+  width: 100% !important;
 }
 
 .radius-style {
